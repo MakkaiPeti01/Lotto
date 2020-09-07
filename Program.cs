e863a76ce8a185da0d2693f711d8686f8ebe3399
MakkaiPeti01 <PetyaPower@gmail.com>
2020-09-07 11:43:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Írj be egy számot ");
+            string be = Console.ReadLine();
+            int[] tomb = new int[5];
+            Random r = new Random();
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                tomb[i] = r.Next(0,91);
+            }
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                Console.Write(tomb[i]+",");
+            }
+            Console.WriteLine();
+            int talalat = 0;
+            bool asd = false;
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if (int.Parse(be)==tomb[i])
+                {
+                    talalat++;
+                    asd = true;
+                }
+            }
+            if (asd==true)
+            {
+                Console.WriteLine("fasza vagy");
+            }
+            Console.WriteLine("Ennyiszer találtad el: {0}",talalat);
+            Console.ReadKey();
+        }
+    }
+}
